45bb55b19b4c1f5f668d56f069fbe5deee51af3a
guille <alufer3712@ieselcaminas.org>
2022-09-12 13:13:16 +0200

    Primero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/articulo.model.ts
@@ -0,0 +1,12 @@
+export class Articulo {
+
+    nombre: string = "";
+    cantidad: number = 0;
+    
+    constructor(nombre: string, cantidad: number) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
+
+}
