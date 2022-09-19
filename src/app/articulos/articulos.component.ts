748df477ff8285499c00b2ede69c41413332fdb5
Guillermo Fernandez <alufer3712@ieselcaminas.org>
2022-09-19 22:00:51 +0200

    Añadir eliminar

--- src/app/articulos/articulos.component.ts
+++ src/app/articulos/articulos.component.ts
@@ -12,13 +12,14 @@ export class ArticulosComponent implements OnInit {
   title = '¡Aquí tienes tu lista de la compra!';
   subtitle = 'Tus artículos:';
 
+  indice: number = 0;
   articulos: Articulo[] = [];
   cuadroNombre: string = "";
   cuadroCantidad: number = 0;
-  
+
 
   constructor() {
-    
+
   }
 
   ngOnInit(): void {
@@ -26,11 +27,16 @@ export class ArticulosComponent implements OnInit {
 
   agregarArticulo() {
     let miArticulo = new Articulo(this.cuadroNombre, this.cuadroCantidad);
-   // alert("El artículo " + miArticulo.nombre + " se añadirá a la lista.");
+    // alert("El artículo " + miArticulo.nombre + " se añadirá a la lista.");
     this.articulos.push(miArticulo);
   }
-  eliminarArticulo(){
-    this.articulos.splice();
+
+  eliminarArticulo(indice: number) {
+    for (let i = 0; i < this.articulos.length; i++) {
+      if (i == indice) {
+        this.articulos.slice(indice, 0);
+      }
+    }
+    return this.articulos;
   }
-  
 }
